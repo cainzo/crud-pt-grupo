8c7c71234ccfaddd5a0bb7928c3dd9fa6c06dc91
GonzaloC <cainzog@gmail.com>
2021-10-22 11:20:38 -0300

    commit con sin la el carrito de compras

---
+++ js/productoClass.js
@@ -0,0 +1,8 @@
+export class Producto{
+    constructor(codigo, producto, descripcion, url){
+    this.codigo = codigo;
+    this.producto = producto;
+    this.descripcion = descripcion;
+    this.url = url;
+    }
+}
